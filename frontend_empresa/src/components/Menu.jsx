d41616572a543320c7508920471e67298c9a370d
. <javiercastillolimon@gmial.com>
2025-02-04 20:02:27 +0100

    Modo oscuro

--- frontend_empresa/src/components/Menu.jsx
+++ frontend_empresa/src/components/Menu.jsx
@@ -12,20 +12,43 @@ import {
   MDBDropdownItem,
   MDBCollapse,
 } from "mdb-react-ui-kit";
-import { useState } from "react";
-import DeviceHubIcon from '@mui/icons-material/DeviceHub';
-import { Link } from "react-router-dom"; 
+import { useState, useEffect } from "react";
+import DeviceHubIcon from "@mui/icons-material/DeviceHub";
+import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 function Menu() {
   const [openBasic, setOpenBasic] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("darkMode") === "true"; // Cargar desde localStorage
+  });
+
+  // Efecto para aplicar el tema al cargar la página
+  useEffect(() => {
+    if (darkMode) {
+      document.body.style.backgroundColor = "#121212";
+      document.body.style.color = "white";
+    } else {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    }
+    localStorage.setItem("darkMode", darkMode); // Guardar preferencia en localStorage
+  }, [darkMode]);
 
   return (
-    <MDBNavbar expand="lg" light bgColor="light">
+    <MDBNavbar expand="lg" light={!darkMode} dark={darkMode} bgColor={darkMode ? "dark" : "light"}>
       <MDBContainer fluid>
         <MDBNavbarBrand href="#">
           <DeviceHubIcon fontSize="large" sx={{ marginRight: 1 }} />
           Software Tremendo Inc
         </MDBNavbarBrand>
+
+        {/* Botón para activar/desactivar modo oscuro */}
+        <IconButton onClick={() => setDarkMode(!darkMode)} sx={{ ml: 2 }}>
+          {darkMode ? <LightModeIcon sx={{ color: "yellow" }} /> : <DarkModeIcon />}
+        </IconButton>
 
         <MDBNavbarToggler
           aria-controls="navbarSupportedContent"
@@ -38,21 +61,20 @@ function Menu() {
 
         <MDBCollapse navbar open={openBasic}>
           <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
-
             {/* Menú de Departamentos */}
             <MDBNavbarItem>
               <MDBDropdown>
                 <MDBDropdownToggle tag="a" className="nav-link" role="button">
                   Departamentos
                 </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <Link to="/altadepartamento" style={{ color: "#4f4f4f", textDecoration: "none" }}>
+                <MDBDropdownMenu style={{ backgroundColor: darkMode ? "#333" : "white" }}>
+                  <Link to="/altadepartamento" style={{ color: darkMode ? "white" : "#4f4f4f", textDecoration: "none" }}>
                     <MDBDropdownItem link>Alta de Departamentos</MDBDropdownItem>
                   </Link>
-                  <Link to="/listadodepartamentos" style={{ color: "#4f4f4f", textDecoration: "none" }}>
+                  <Link to="/listadodepartamentos" style={{ color: darkMode ? "white" : "#4f4f4f", textDecoration: "none" }}>
                     <MDBDropdownItem link>Listado de Departamentos</MDBDropdownItem>
                   </Link>
-                  <Link to="/buscardepartamento" style={{ color: "#4f4f4f", textDecoration: "none" }}>
+                  <Link to="/buscardepartamento" style={{ color: darkMode ? "white" : "#4f4f4f", textDecoration: "none" }}>
                     <MDBDropdownItem link>Buscar Departamento</MDBDropdownItem>
                   </Link>
                 </MDBDropdownMenu>
@@ -65,20 +87,19 @@ function Menu() {
                 <MDBDropdownToggle tag="a" className="nav-link" role="button">
                   Empleados
                 </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <Link to="/altaempleado" style={{ color: "#4f4f4f", textDecoration: "none" }}>
+                <MDBDropdownMenu style={{ backgroundColor: darkMode ? "#333" : "white" }}>
+                  <Link to="/altaempleado" style={{ color: darkMode ? "white" : "#4f4f4f", textDecoration: "none" }}>
                     <MDBDropdownItem link>Alta de Empleados</MDBDropdownItem>
                   </Link>
-                  <Link to="/listadoempleado" style={{ color: "#4f4f4f", textDecoration: "none" }}>
+                  <Link to="/listadoempleado" style={{ color: darkMode ? "white" : "#4f4f4f", textDecoration: "none" }}>
                     <MDBDropdownItem link>Listado de Empleado</MDBDropdownItem>
                   </Link>
-                  <Link to="/buscarempleado" style={{ color: "#4f4f4f", textDecoration: "none" }}>
+                  <Link to="/buscarempleado" style={{ color: darkMode ? "white" : "#4f4f4f", textDecoration: "none" }}>
                     <MDBDropdownItem link>Buscar Empleado</MDBDropdownItem>
                   </Link>
                 </MDBDropdownMenu>
               </MDBDropdown>
             </MDBNavbarItem>
-
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBContainer>
